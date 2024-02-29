7fff8e692b1752f67e5dc344cc183675dff0b685
Yulia Pipka <yuliaziliwu59@sma.belajar.id>
2024-02-29 16:13:54 +0700

    penambahan data controller, view, dan route oleh yulia

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,24 @@
 use App\Http\Controllers\ListBarangController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DataController;
 use App\Http\Controllers\DashboardController;
 
 // Route::get('/', function () {
 //      return view('welcome');
 // });
-Route::get('login', function () {
-    return view('login');
-});
+Route::get('login', function () {return view('login');});
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return 'Admin Dashboard';
-    });
-    Route::get('/users', function () {
-        return 'Admin Users';
-    });
-});
-Route::get('/user/{id}', function ($id) {
-    return 'User dengan ID '. $id;
-});
-//Route::get('/listbarang/{id}/{nama}', function ($id,$nama) {
+Route::get('/welcome', function () {return view('welcome');});
+
+Route::prefix('admin')->group(function () {Route::get('/dashboard', function () {return 'Admin Dashboard';});
+
+Route::get('/users', function () {return 'Admin Users';});});
+
+Route::get('/user/{id}', function ($id) {return 'User dengan ID '. $id;});
+// Route::get('/listbarang/{id}/{nama}', function ($id,$nama) {
 //    return view('list_barang', compact('id', 'nama'));
-//});
+// });
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/refresh-data', [DashboardController::class, 'refreshData']);
@@ -36,3 +28,4 @@ Route::get('/refresh-data', [DashboardController::class, 'refreshData']);
 Route::get('listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/dataku', [DataController::class, 'tampilkan']);
