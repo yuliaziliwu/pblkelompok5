2cd1f9e2180060a35d287670b15b7731160582ca
Yulia Pipka <yuliaziliwu59@sma.belajar.id>
2024-03-13 22:56:07 +0700

    perubahan dari yulia

--- resources/views/dashboard_pbl5.blade.php
+++ resources/views/dashboard_pbl5.blade.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="/style/style.css">
+    <link rel=”stylesheet” href="/style/tailwindcss3.4.1.js">
     <script src="https://cdn.tailwindcss.com"></script>
     <body>
         <div class="w-236 h-100 bg-sky-100 opacity-2 mx-0 mr-0 mt-0 pt-6 px-8 pt-8">
--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,15 @@ use App\Http\Controllers\DashboardController;
 //      return view('welcome');
 // });
 Route::get('login', function () {return view('login');});
-
 Route::get('/welcome', function () {return view('welcome');});
-
 Route::prefix('admin')->group(function () {Route::get('/dashboard', function () {return 'Admin Dashboard';});
-
 Route::get('/users', function () {return 'Admin Users';});});
-
 Route::get('/user/{id}', function ($id) {return 'User dengan ID '. $id;});
 // Route::get('/listbarang/{id}/{nama}', function ($id,$nama) {
 //    return view('list_barang', compact('id', 'nama'));
 // });
-
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/refresh-data', [DashboardController::class, 'refreshData']);
-
 Route::get('listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
