3d2f18b47ff3a54014143294ba10cb9c05a95ec0
Yulia Pipka <yuliaziliwu59@sma.belajar.id>
2024-03-13 13:55:14 +0700

    dashboard yulia

--- resources/views/dashboard_pbl5.blade.php
+++ resources/views/dashboard_pbl5.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
+ <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-</head>
-<body>
+    <link rel="stylesheet" href="/style/style.css">
+    
+    <body>
     <h1>Welcome to the Dashboard</h1>
     <p>Data: <span id="dataDisplay">{{ $data }}</span></p>
     <button id="refreshButton">Refresh Data</button>
@@ -12,7 +14,6 @@
     <img src="{{ asset('images/yulia1.jpg') }}" alt="images" style="width:40%">
     <img src="{{ asset('images/yulia2.jpg') }}" alt="images" style="width:40%">
 </div>
-
     <script src="{{ asset('js/dashboard.js') }}"></script>
 </body>
 </html>
